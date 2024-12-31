2b21b77a7129465b99160b97a48c15afd5053fb4
kfujii-6jo <kfujii.dev@gmail.com>
2024-12-31 14:07:44 +0900

    データ取得処理のサンプルを追加

--- prtl-base-client/src/pages/Home.tsx
+++ prtl-base-client/src/pages/Home.tsx
@@ -1,13 +1,36 @@
 import { Button } from "@/components/ui/button";
-import type React from "react";
+import React, { useState, useEffect } from "react";
 import { Layout } from "@/components/layout";
 
+type Post = {
+	id: number;
+	title: string;
+};
+
 export const Home: React.FC = () => {
+	const [posts, setPosts] = useState<Post[]>([]);
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+			.then((res) => res.json())
+			.then((data) => {
+				setPosts(data);
+			});
+	}, []);
 	return (
 		<Layout>
 			<div>
 				<h1 className="text-3xl font-bold underline">Home</h1>
 				<Button className="mt-3">Click me</Button>
+				<ul>
+					{" "}
+					{posts.map((post) => (
+						<li key={post.id}>
+							{post.id}
+							{post.title}
+						</li>
+					))}{" "}
+				</ul>
 			</div>
 		</Layout>
 	);
